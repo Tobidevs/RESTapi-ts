36548e54d87746f3623f6ac7c8e651eb811ed8fb
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-05-04 09:29:55 -0500

    Declared basic routes

---
+++ routes/subscribers.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+
+
+// Getting all
+router.get('/', (req, res, next) => {
+
+})
+// Getting one
+router.get('/:id', (req, res, next) => {
+
+})
+// Creating one
+router.post('/', (req, res, next) => {
+
+})
+// Updating one
+router.patch('/:id', (req, res, next) => {
+
+})
+// Deleting one
+router.delete('/:id', (req, res, next) => {
+
+})
+
+module.exports = router
