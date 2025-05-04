c10b1a94a70887830810e2c6881789b15c98172d
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-05-03 21:30:43 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('dotenv').config()
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'));
+
+
+
+app.listen(3000, () => console.log('Server Started'))
