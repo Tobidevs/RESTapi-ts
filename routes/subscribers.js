fd0ac77ff72fc9af706ee41f2faee6471a92e0bb
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-05-05 15:44:38 -0500

    defined patch and delete routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res, next) => {
 })
 // Getting one
 router.get('/:id', getSubscriber, (req, res, next) => {
-  res.send(res.subscriber.name)
+  res.json(res.subscriber)
 })
 // Creating one
 router.post('/', async (req, res, next) => {
@@ -29,12 +29,29 @@ router.post('/', async (req, res, next) => {
   }
 })
 // Updating one
-router.patch('/:id', getSubscriber, (req, res, next) => {
-
+router.patch('/:id', getSubscriber, async (req, res, next) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel
+  }
+  try {
+    const updatedSubscriber = await res.subscriber.save()
+    res.json(updatedSubscriber)
+  } catch(err) {
+    res.status(400).json({ message: err.message})
+  }
 })
 // Deleting one
-router.delete('/:id', getSubscriber, (req, res, next) => {
+router.delete('/:id', getSubscriber, async (req, res, next) => {
+  try {
+    await res.subscriber.deleteOne();
+    res.json({ message: 'Deleted Subscriber'})
+  } catch(err) {
+    res.status(500).json({ message: err.message})
 
+  }
 })
 
 async function getSubscriber(req, res, next) {
